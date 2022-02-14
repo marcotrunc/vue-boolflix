4dea0055d068bc55d3689a3be70edc4953cda8eb
marcotrunc <marcotrunc@gmail.com>
2022-02-14 18:30:24 +0100

    add the flags component

---
+++ src/components/Flag.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <img :src="changeFlag(language)" :alt="language" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Flag",
+  props: ["language"],
+  data() {
+    return {
+      flags: [
+        {
+          langString: "it",
+          langUrl: "../assets/img/it.png",
+        },
+        {
+          langString: "en",
+          langUrl: "../assets/img/en.png",
+        },
+        {
+          langString: "not-found",
+          langUrl: "../assets/img/not.png",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeFlag(text) {
+      let url = require("../assets/img/not.png");
+      //   if I add a flag later, I don't have to change anything
+      this.flags.forEach((flag) => {
+        if (flag.langString.includes(text)) {
+          url = require(`../assets/img/${text}.png`);
+        }
+      });
+      return url;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped >
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,9 @@
     <ul v-for="film in films" :key="film.id || index">
       <h3>{{ film.title }}</h3>
       <li>{{ film.original_title }}</li>
-      <li>{{ film.original_language }}</li>
+      <li>
+        <Flag :language="film.original_language" />
+      </li>
       <li>{{ film.vote_average }}</li>
     </ul>
   </div>
@@ -11,13 +13,16 @@
 
 <script>
 import axios from "axios";
+import Flag from "./Flag.vue";
 export default {
   name: "Main",
   props: ["search"],
+  components: {
+    Flag,
+  },
   data() {
     return {
       films: [],
-
       //   data Api
       apiKey: "0716b96a2396251891c3d483c41f9ebc",
       language: "it-IT",
@@ -37,13 +42,13 @@ export default {
         .get("https://api.themoviedb.org/3/search/movie", config)
         .then((res) => {
           this.films = res.data.results;
-          console.log(this.films);
         });
     },
   },
   computed: {
     searchFilms() {
-      return this.fetchFilms();
+      if (!this.search) return;
+      else return this.fetchFilms();
     },
   },
 };
